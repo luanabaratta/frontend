4a578a726d6fbd2bc699bea4c355aaeeb79812f7
Luana B. dos Santos <luanabarata.technology@gmail.com>
2022-12-14 15:03:09 -0300

    ...

---
+++ studies/html-css-js/Alura/aluramidi/main.js
@@ -0,0 +1,33 @@
+function tocaSom(idElementoTocado) {
+    const elemento = document.querySelector(idElementoTocado);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
+    
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = () => {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = (evento) => {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = () => {
+        tecla.classList.remove('ativa');
+    }
+}
